1afd6325c06f31e8a249be112b3c4410d03f29c3
Bruno Assis <bruno.assis132@gmail.com>
2021-03-16 15:31:55 -0300

    :+1: create forms and source changes

--- DelphiToHero.dpr
+++ DelphiToHero.dpr
@@ -5,8 +5,9 @@ uses
   DelphiToHero.View.Main in 'src\view\DelphiToHero.View.Main.pas' {frmPrincipal},
   DelphiToHero.View.Styles.Colors in 'src\view\styles\DelphiToHero.View.Styles.Colors.pas',
   DelphiToHero.View.Pages.Principal in 'src\view\pages\DelphiToHero.View.Pages.Principal.pas' {pagePrincipal},
-  DelphiToHero.View.Pages.Usuarios in 'src\view\pages\DelphiToHero.View.Pages.Usuarios.pas' {pageUsuarios},
-  DelphiToHero.View.Routers in 'src\view\routers\DelphiToHero.View.Routers.pas';
+  DelphiToHero.View.Routers in 'src\view\routers\DelphiToHero.View.Routers.pas',
+  DelphiToHero.View.Pages.Form.Template in 'src\view\pages\DelphiToHero.View.Pages.Form.Template.pas' {frmTemplate},
+  DelphiToHero.View.Pages.Usuarios in 'src\view\pages\DelphiToHero.View.Pages.Usuarios.pas' {pageUsuarios};
 
 {$R *.res}
 
@@ -15,5 +16,7 @@ begin
   ReportMemoryLeaksOnShutdown := DebugHook <> 0;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmTemplate, frmTemplate);
+  Application.CreateForm(TpageUsuarios, pageUsuarios);
   Application.Run;
 end.
