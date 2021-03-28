35764fead6420e4f2d1f0bbd7168f27c32f06c42
Bruno <53619830+sklorde@users.noreply.github.com>
2021-03-28 19:42:29 -0300

    :fire: remove FireDAC dependencies

--- src/view/pages/DelphiToHero.View.Pages.Usuarios.pas
+++ src/view/pages/DelphiToHero.View.Pages.Usuarios.pas
@@ -22,15 +22,6 @@ uses
   Data.DB,
   Vcl.Grids,
   Vcl.DBGrids,
-  FireDAC.Stan.Intf,
-  FireDAC.Stan.Option,
-  FireDAC.Stan.Param,
-  FireDAC.Stan.Error,
-  FireDAC.DatS,
-  FireDAC.Phys.Intf,
-  FireDAC.DApt.Intf,
-  FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client,
   Vcl.WinXPanels,
   DelphiToHero.View.Styles.Colors;
 
